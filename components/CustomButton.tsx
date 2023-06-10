0b66d5e8a35f4cd2fb23f522b9b2bdaa7a6d93e1
frdrk00 <masterkooo@gmail.com>
2023-06-10 19:11:47 +0200

    RAPID API Cars

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,13 +1,14 @@
 'use client';
 
+import { FC } from 'react';
 import { CustomButtonProps } from '@/types';
 
-const CustomButton = ({
+const CustomButton: FC<CustomButtonProps> = ({
   title,
   containerStyles,
   handleClick,
   btnType,
-}: CustomButtonProps) => (
+}) => (
   <button
     disabled={false}
     type={btnType || 'button'}
---
+++ components/CustomFilter.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface CustomFilterProps {
+  
+}
+
+const CustomFilter: FC<CustomFilterProps> = ({}) => {
+  return <div>CustomFilter</div>
+}
+
+export default CustomFilter
